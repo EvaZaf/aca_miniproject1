59d243450862b2e2656b330643fbfb61bc24186a
EvaZaf <evazaf1995@gmail.com>
2019-10-24 16:44:52 +0100

    dummy change in Menu.java class.

--- src/main/java/MiniProject1/Menu.java
+++ src/main/java/MiniProject1/Menu.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 public class Menu {
 	public static void main(String[] args) {
-		System.out.println("Please insert the keyword:");
+		//System.out.println("Please insert the keyword:");
 		Scanner keyword = new Scanner(System.in);
 		//connectdb (keyword);
 	}	
